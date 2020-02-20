ec8675da4cd5ab6f8033d22fb6889a530f187dc2
Jesuva <52796986+Jesuva@users.noreply.github.com>
2020-02-20 10:51:52 +0530

    Update main.js

--- GUVI/script/main.js
+++ GUVI/script/main.js
@@ -3,12 +3,12 @@ function myFunction() {
     var email = document.getElementById("userMail").value;
     var password = document.getElementById("userPassword").value;
 
-    // Returns successful data submission message when the entered information is stored in database.
+    
     var dataString = 'name1=' + name + '&email1=' + email + '&password1=' + password;
     if (name == '' || email == '' || password == '') {
         alert("Please Fill All Fields");
     } else {
-        // AJAX code to submit form.
+        
         $.ajax({
             type: "POST",
             url: "signup.php",
